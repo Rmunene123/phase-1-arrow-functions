1ce85ce9adaca836b46802ed97ae555dd74881c5
Rmunene123 <rosewanmun@gmail.com>
2024-07-02 14:38:10 +0300

    added arrow functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+function divide(a, b) {
+    return a / b;
+  }
+  
+  const square = (x) => {
+    return x * x;
+  };
+  
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
